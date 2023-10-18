5cc67f8552ddc80ffcd726da97cf772e71ae7dcc
Abe-alt <yousfi.abdou@gmail.com>
2023-10-18 11:27:15 +0100

    func min w/ argv validation

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('hello abe !')
+const { argv } = require("process")
+
+function main(){
+    if (process.argv.length < 3) {
+        console.log('no website provided !')
+        process.exit(1)
+    } else if (process.argv.length > 3){
+        console.log('wrong number of args !')
+        process.exit(1)
+    }
+    console.log(`Crawler is starting at : ${process.argv[2]}`)
+
+}
+
+main()
